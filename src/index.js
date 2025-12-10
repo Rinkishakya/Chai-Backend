fd99b5ae51ed35d6c6cced3613d0b36b4ed46182
Rinki <rinkishakya270@gmail.com>
2025-12-10 14:08:52 +0530

    add line  index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log("Hello from Node.js with ES Modules!");
+import mongoose from "mongoose";
+import {DB_NAME} from "./constant";
+
+import express from "express";
+const app = express()
+
+(async () => {
+    try {
+        await  mongoose.connect(`${process.env.MONGODB_URL} /${DB_NAME}`)
+        // Start the Express server after successful DB connection
+        app.on("error", (err) => {
+            console.log("Express server error:", err);
+            throw err
+        });
+        
+    } catch (error) {
+        console.error("Error connecting to the database:", error);
+        throw err
+    }
+})()
